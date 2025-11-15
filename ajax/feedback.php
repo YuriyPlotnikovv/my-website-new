f2489b7e8d93735c108124608f47ffc295f84948
Yuriy Plotnikov <yuriy.plotnikov@travelline.ru>
2025-11-15 23:32:37 +0300

    Add feedback form and class autoloader

--- ajax/feedback.php
+++ ajax/feedback.php
@@ -1,8 +1,7 @@
 <?php
+require_once __DIR__ . '/../core/classes/FormHelper.php';
 
 use core\FormHelper;
-
-require_once __DIR__ . '/../core/autoload.php';
 
 $form = new FormHelper(true);
 $form->help();
--- core/updateCacheFiles.php
+++ core/updateCacheFiles.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/apiKeys.php';
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/classes/Tools.php';
+require_once __DIR__ . '/classes/VkPhotoFetcher.php';
+require_once __DIR__ . '/classes/GitHubProjectsFetcher.php';
 
 use core\VkPhotoFetcher;
 use core\GitHubProjectsFetcher;
